2d177658e2831c557b88248d21678491c93c827b
Ahmed Mahmoud <97903655+AhmedMahmoud929@users.noreply.github.com>
2024-12-06 11:25:07 +0200

    Update geminiConfig.ts

--- src/constants/geminiConfig.ts
+++ src/constants/geminiConfig.ts
@@ -6,7 +6,7 @@ export const geminiAPIKey = "AIzaSyCNWah97hLHBBw9vjGKgnqq5ogambbKAzs";
 export const geminiSystemInstructions = {
   randomWordGenerator: `Generate a random English word suitable for the user's proficiency level and interest. Provide a simple definition, three example sentences, synonyms, antonyms, and highlight its usage in different contexts.`,
   wordFinder: `Help the user explore a specific word. Provide a simple definition, three example sentences, synonyms, antonyms, and highlight its usage in different contexts. Use very easy way and English level, use A1 level in description and A2 in examples`,
-  compAssessmenter: `I'm trying to gain some vocab, so I've just learned some new words. I want to ensure that I have understood them well, So I'll provide you with the word and an example of it, and I want you to assess my comprehension of this word and give me a degree from 100 and give me a PASS or FAIL based on it, please tell me and teach me about it. Make your response ain't too long. And also do not be so hard, be friendly but realistic`,
+  compAssessmenter: `I'm trying to gain some vocab, so I've just learned some new words. I want to ensure that I have understood them well, So I'll provide you with the word and an example of it, and I want you to assess my comprehension of this word and give me a degree from 100 and give me a PASS or FAIL based on it, please tell me and teach me about it. Make your response ain't too long. And also do not be so hard, be friendly but realistic. Also if you felt if I got the full comprehension give me the full degree, do not be so cruel if I'm right`,
 };
 
 export const geminiPrompts = {
